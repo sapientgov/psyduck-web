edeb758866d4cdf72262c15ec8bc087dcdd0552c
Sarah Lamont <sarah@sarahlamont.com>
2016-07-13 14:41:15 -0400

    layout updates to staff page

--- app/components/staff-view/staff-view.jsx
+++ app/components/staff-view/staff-view.jsx
@@ -23,23 +23,28 @@ export class StaffView extends Component {
 
   render() {
     return (
-      <div className="uikit usa-grid">
-        <p>Logged in as: {this.props.user}</p>
-        <h3>Appointment Dashboard</h3>
-        <div className="usa-width-one-whole add-hmargin schedule-step">
-					<h3>Next available appointments for XX/XX/XXXX</h3>
-					<span className="label">Select a different day:</span>
-					<DatePicker  placeholderText="Click to select a date" selected={moment(this.state.date, 'YYYY-MM-DD')}
-            dateFormat="YYYY-MM-DD"
-            onChange={this.handleChange} />
-          <img className="calendar-icon" src="/static-assets/calendar-icon24x24.png" />
-				</div>
-        <div className="scheduleapt">
-          <AvailableAppointments date={this.state.date}/>
+      <div>
+        <div className="usa-width-one-whole section-header">
+            <h2>Logged in as <b>{this.props.user}</b></h2>
         </div>
-        <div>
-          <button onClick="javascript:window.print();">Print</button>
+        <div className="uikit usa-grid  scheduleapt">
+          <h3>Appointment Dashboard</h3>
+          <div className="usa-width-one-whole schedule-step">
+  					<h3>Next available appointments for XX/XX/XXXX</h3>
+  					<span className="label">Select a different day:</span>
+  					<DatePicker  placeholderText="Click to select a date" selected={moment(this.state.date, 'YYYY-MM-DD')}
+              dateFormat="YYYY-MM-DD"
+              onChange={this.handleChange} />
+            <img className="calendar-icon" src="/static-assets/calendar-icon24x24.png" />
+  				</div>
+          <div className="scheduleapt">
+            <AvailableAppointments date={this.state.date}/>
+          </div>
+          <div>
+            <button onClick="javascript:window.print();">Print</button>
+          </div>
         </div>
+
       </div>
     );
   }
