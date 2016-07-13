597a9bc511a5243e7369de621a493b25deff5186
Sarah Lamont <sarah@sarahlamont.com>
2016-07-13 13:58:14 -0400

    final style changes to appointment confirmation page

--- app/index.jsx
+++ app/index.jsx
@@ -8,6 +8,7 @@ import App from './components/app';
 import Uikit from './components/uikit';
 import ScheduleApt from './components/scheduleapt';
 import ScheduleConfirmation from './components/scheduleconfirmation';
+import AppointmentDetails from './components/appointmentdetails';
 import Login from './components/login/login';
 import StaffView from './components/staff-view/staff-view';
 
@@ -17,6 +18,7 @@ ReactDOM.render(
     <Route path="/" component={App}>
       <Route path="uikit" component={Uikit}/>
       <Route path="scheduleconfirmation" component={ScheduleConfirmation}/>
+      <Route path="appointmentdetails" component={AppointmentDetails} />
       <Route path="scheduleapt" component={ScheduleApt}/>
       <Route path="login" component={Login}/>
       <Route path="staff" component={StaffView}/>
