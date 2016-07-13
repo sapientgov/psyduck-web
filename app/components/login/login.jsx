b75322fe137110e37543def0e8c6eb90ad72a1c0
Christopher Perry <cperry@sapient.com>
2016-07-13 11:44:09 -0400

    finalizing validation

--- app/components/login/login.jsx
+++ app/components/login/login.jsx
@@ -26,19 +26,22 @@ export class Login extends Component {
         <div className={!this.state.username && this.state.usernameFocused ? 'usa-input-error' : ''}>
           <label htmlFor="txt-username">Username</label>
           {!this.state.username && this.state.usernameFocused ?
-            <span className="usa-input-error-message" id="input-error-message" role="alert">Helpful error message</span>
+            <span className="usa-input-error-message" id="input-error-message" role="alert">Please enter a username</span>
             : null}
-            <input ref="username" id="txt-username" name="username" type="text"
+            <input id="txt-username" name="username" type="text"
             value={this.state.username.value}
-            onFocus={() => this.setState({usernameFocused: true})}
+            onBlur={() => this.setState({usernameFocused: true})}
             onChange={(e) => this.setState({username: e.target.value})} />
         </div>
 
         <div className={!this.state.password && this.state.passwordFocused ? 'usa-input-error' : ''}>
           <label htmlFor="txt-password">Password</label>
+          {!this.state.password && this.state.passwordFocused ?
+            <span className="usa-input-error-message" id="input-error-message" role="alert">Please enter a password</span>
+            : null}
           <input id="txt-password" name="password" type="password"
             value={this.state.password.value}
-            onFocus={() => this.setState({passwordFocused: true})}
+            onBlur={() => this.setState({passwordFocused: true})}
             onChange={(e) => this.setState({password: e.target.value})} />
         </div>
       <button onClick={this.onSubmit}>Login</button>
