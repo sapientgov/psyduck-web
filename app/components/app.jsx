edd32e3183e13bae9c90db38425e823606ab53e4
Christopher Perry <cperry@sapient.com>
2016-07-13 09:35:40 -0400

    removing uneeded boilerplate

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -3,7 +3,6 @@ import {Provider} from 'react-redux';
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import CounterReducer from '../reducers/counter-reducer';import AppReducer from '../reducers/app-reducer';
 import ErrorReducer from '../reducers/error-reducer';
 import ConfigReducer from '../reducers/config-reducer';
 import {Link} from 'react-router';
@@ -11,8 +10,6 @@ import {configureApp} from '../actions/config-actions';
 
 //combine reducers
 const appReducers = combineReducers({
-  counter: CounterReducer,
-  app: AppReducer,
   error: ErrorReducer,
   config: ConfigReducer
 });
@@ -39,11 +36,7 @@ export default class App extends Component {
               <ul>
                 <li><Link activeClassName="activeLink" to="/">Home</Link></li>
                 <li><Link className="mainNavLink" activeClassName="activeLink" to="/uikit">UI Kit</Link></li>
-                <li><Link className="mainNavLink" activeClassName="activeLink" to="/heroes">Heroes</Link></li>
-                <li><Link className="mainNavLink" activeClassName="activeLink" to="/counter">Counter</Link></li>
                 <li><Link className="mainNavLink" activeClassName="activeLink" to="/login">Login</Link></li>
-                <li><Link className="mainNavLink" activeClassName="activeLink" to="/details">Details</Link></li>
-                <li><Link className="mainNavLink" activeClassName="activeLink" to="/form">Form</Link></li>
               </ul>
             </nav>
           <main>
